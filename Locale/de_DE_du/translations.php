66eb8c678cd3f2be55d67bdb23170e6b279a13e9
manne65-hd <manne@hoffmaenner.net>
2021-04-10 07:36:23 +0200

    Prepare compatibility with the upcoming KB-release, that will have the new language "Deutsch (du)"

---
+++ Locale/de_DE_du/translations.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+    'Allows boards to be sorted by different dates(Date due, started, created, modified, moved, closed) or in kanboard default-mode' => 'Ermöglicht die Pinnwand nach verschiedenen Datumsfeldern zu sortieren(Fälligkeit, Startdatum, Erstellungsdatum, Änderungsdatum, Verschiebe-Datum, Schliess-Datum) oder in der Kanboard Standard-Sortierung',
+    'Sort-settings for the project-board' => 'Sortier-Einstellungen für die Projekt-Pinnwand',
+    'Board sort-settings' => 'Board-Sortierung',
+    'Sort method' => 'Sortier-Methode',
+    'Show tasks in board order (Kanboard Default-method)' => 'Zeige Aufgaben in Board-Reihenfolge (Kanboard Standard-Methode)',
+    'Show tasks sorted by due date' => 'Zeige Aufgaben sortiert nach Fälligkeitsdatum an',
+    'Show tasks sorted by start date' => 'Zeige Aufgaben sortiert nach Startdatum an',
+    'Show tasks sorted by the date they were created' => 'Zeige Aufgaben sortiert nach dem Erstellungsdatum an',
+    'Show tasks sorted by the date they were last modified' => 'Zeige Aufgaben sortiert nach dem Datum der letzten Änderung an',
+    'Show tasks sorted by the date they were last moved' => 'Zeige Aufgaben sortiert nach dem Datum, an dem sie zuletzt verschoben wurden',
+    'Show tasks sorted by the date they were closed' => 'Zeige Aufgaben sortiert nach dem Datum an dem sie geschlossen wurden',
+    'Sort method for other tasks' => 'Sortier-Methode für andere Aufgaben',
+    'Show other tasks BEFORE the tasks sorted by date' => 'Zeige andere Aufgaben VOR den nach Datum sortierten Aufgaben an',
+    'Show other tasks AFTER the tasks sorted by date' => 'Zeige andere Aufgaben NACH den nach Datum sortierten Aufgaben an',
+    'Defines where to display tasks, that have no date-information for the method selected above' => 'Legt fest, wo Aufgaben angezeigt werden, die keine Datums-Informationen zur oben gewählten Sortier-Methode enthalten',
+    'Has no effect, if sort-method is set to "board order"' => 'Hat keine Auswirkungen, wenn die Sortier-Methode auf "Board-Reihenfolge" eingestellt ist',
+    'TASKS > Sorted by due date' => 'AUFGABEN > Sortiert nach Fälligkeitsdatum',
+    'TASKS > Sorted by start date' => 'AUFGABEN > Sortiert nach Startdatum',
+    'TASKS > Sorted by the date they were created' => 'AUFGABEN > Sortiert nach dem Erstellungsdatum',
+    'TASKS > Sorted by the date they were last modified' => 'AUFGABEN > Sortiert nach dem Datum der letzten Änderung',
+    'TASKS > Sorted by the date they were last moved' => 'AUFGABEN > Sortiert nach dem Datum, an dem sie zuletzt verschoben wurden',
+    'TASKS > Sorted by the date they were closed' => 'AUFGABEN > Sortiert nach dem Datum an dem sie geschlossen wurden'
+);
